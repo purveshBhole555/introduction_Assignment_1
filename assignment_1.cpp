b5108bbd896473a0942e495a97643aa5b39ac110
Purvesh bhole <purveshbhole426@gmail.com>
2024-06-04 12:15:13 +0530

    first commit

---
+++ assignment_1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Q1
+    cout<<"Physics"<<endl;
+    cout<<"Wallah"<<endl;
+
+    // Q2
+    cout<<5+5;
+
+    // Q3
+    int = 4 bytes
+    bool = 1 byte
+    float = 4 bytes
+
+    // Q4
+    
+    int a = 4;
+    int b = 5;
+    a++, b--;
+    cout << ++a << " " << b--;
+    output : 6 4
+
+    // Q5
+    cout<<"The circumference of the circle with radius 10 is "<<2*3.14*10;
+
+    // Q6
+    01Pwskills   --> not a variable
+    _FLOAT   --> a variable
+    int   --> not a variable
+    FLOAT   --> a variable
+    You will succeed  --> not a variable
+
+
+
+
+    return 0;
+}
+
+
